eda07f53f365447d9cc6d2ce13ef30e82f896c7e
root <arruda.1@icloud.com>
2022-11-22 00:12:38 +0000

    apache v.12

---
+++ apache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo " Atualizando server 1 !"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo " baixando.....!"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
